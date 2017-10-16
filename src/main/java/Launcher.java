9cad2e9c3ebbe2b88b092dc4b796815fae49689e
Robert Levenbach <R.W.Levenbach@student.tudelft.nl>
2017-10-16 15:16:24 +0200

    second commit

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -3,9 +3,23 @@
  */
 public class Launcher {
 
-    public static void main(String[] args){
-        System.out.println("Hoi");
+
+
+    public static void main(String[] args) {
+        int age=3;
+        switch (age) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+        }
     }
+
 
 
 }
